feedd49f6ca59afb5153909ee71f74069db87f4f
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-03-27 01:21:36 +0800

    feat: large file catch size.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import withPWAInit from '@ducanh2912/next-pwa';
+import withPWAInit, { runtimeCaching } from '@ducanh2912/next-pwa';
 
 /**
  * @type {import('next').NextConfig}
@@ -10,7 +10,16 @@ const nextConfig = {
 };
 
 const withPWA = withPWAInit({
+  cacheOnFrontendNav: true,
+  aggressiveFrontEndNavCaching: true,
+  cacheStartUrl: true,
+  disable: process.env.NODE_ENV === 'development',
   dest: 'public',
+  scope: '/',
+  register: true,
+  reloadOnOnline: false,
+  skipWaiting: true,
+  maximumFileSizeToCacheInBytes: 3000000,
 });
 
 export default withPWA({ nextConfig });
