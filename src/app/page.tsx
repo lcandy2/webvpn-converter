97b721971aeb4c0a373dfff42226effb4305849c
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-03-27 02:50:35 +0800

    feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,25 @@ import InitSelectSchool from '@/app/_libs/components/init-select-school';
 import { TitleComponent } from '@/app/_libs/components/title';
 import UrlConverter from '@/app/_libs/components/url-converter/url-converter';
 import HomeStatus from '@/app/_libs/components/home-status';
+import '@/../package.json';
+import { getPackageVersion } from 'next/dist/lib/get-package-version';
 
 export default function Home() {
   return (
-    <>
-      <InitSelectSchool />
-      <TitleComponent title="转换" marginBottom={false} />
-      <HomeStatus />
-      <UrlConverter />
-    </>
+    <div className="flex flex-col min-h-[100%]">
+      <section className="grow">
+        <InitSelectSchool />
+        <TitleComponent title="转换" marginBottom={false} />
+        <HomeStatus />
+        <UrlConverter />
+      </section>
+      <footer className="text-center text-xs text-gray-500 flex flex-row flex-wrap divide-x">
+        <p className="px-2">MIT License</p>
+        <p className="px-2">
+          甜檸Cirtron🍋 &copy; 2023 - {new Date().getFullYear()}
+        </p>
+        <p className="px-2">Privacy</p>
+      </footer>
+    </div>
   );
 }
