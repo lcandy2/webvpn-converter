74a174a626dabf9167d5458bd5c068c1d315ebe6
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-04-01 20:41:57 +0800

    feat: error reslove.

---
+++ src/app/error.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useCallback, useEffect } from 'react';
+import Title from '@/app/_libs/components/title';
+import Link from 'next/link';
+import MdFab from '@/app/_libs/ui/floating-action-buttons';
+import HomeIcon from '@mui/icons-material/Home';
+import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import { useRouter } from 'next/navigation';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  const handleCleanButtonClick = useCallback(() => {
+    localStorage.clear();
+    router.refresh();
+    reset();
+  }, [reset]);
+
+  return (
+    <div>
+      <Title title="Error" subtitle={`错误：${error}`} />
+      <MdFab
+        label="清除数据（推荐）"
+        className="w-[100%] sm:w-36 mb-8 sm:mr-8 sm:mb-0"
+        onClick={handleCleanButtonClick}
+      >
+        <span slot="icon">
+          <CleaningServicesIcon />
+        </span>
+      </MdFab>
+      <Link href={'/'}>
+        <MdFab
+          label="返回首页"
+          className="w-[100%] sm:w-36 mb-8 sm:mr-8 sm:mb-0"
+        >
+          <span slot="icon">
+            <HomeIcon />
+          </span>
+        </MdFab>
+      </Link>
+    </div>
+  );
+}
